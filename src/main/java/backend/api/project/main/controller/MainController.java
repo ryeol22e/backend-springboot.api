b97bf53b829d3f9b234641e62751d59133a9f136
ryeol22e <ryeol22e@gmail.com>
2021-11-23 21:07:46 +0900

    add maincotroller

---
+++ src/main/java/backend/api/project/main/controller/MainController.java
@@ -0,0 +1,20 @@
+package backend.api.project.main.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+	
+	@GetMapping
+	public ResponseEntity<?> mainData() {
+		Map<String, Object> resultMap = new HashMap();
+		
+		return ResponseEntity.ok(resultMap);
+	}
+
+}
