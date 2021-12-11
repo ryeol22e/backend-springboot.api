eecb8a0d821f98ed7159c0d89c9d310627ea2c53
ryeol22e <ryeol22e@gmail.com>
2021-12-11 19:41:12 +0900

    add @slf4j 세팅

--- src/main/java/backend/api/project/config/WebConfig.java
+++ src/main/java/backend/api/project/config/WebConfig.java
@@ -2,16 +2,27 @@ package backend.api.project.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 	
 	public WebConfig() {
 		// TODO Auto-generated constructor stub
-		System.out.println(":::::::::::backend api config open:::::::::::");
+		log.info("backend config start...");
 	}
 	
-	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		// TODO Auto-generated method stub
+		registry
+			.addMapping("/**")
+			.allowedOrigins("http://localhost:8080");
+		WebMvcConfigurer.super.addCorsMappings(registry);
+	}
 
 }
--- src/main/java/backend/api/project/main/controller/MainController.java
+++ src/main/java/backend/api/project/main/controller/MainController.java
@@ -7,6 +7,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 public class MainController {
 	
