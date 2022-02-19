ffb7b8c2f8ad941420cee39e3f4acde96f0b5e2c
ryeol22e <ryeol22e@gmail.com>
2022-02-20 00:29:29 +0900

    jasyptConfig setting

---
+++ src/main/java/backend/api/project/config/JasyptConfig.java
@@ -0,0 +1,30 @@
+package backend.api.project.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfig {
+	
+	@Bean("jasyptSringEncryptor")
+	public StringEncryptor stringEncrtyptor() {
+		String key = new String("my_jasypt_key");
+		PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+		SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+		
+		config.setPassword(key);
+		config.setAlgorithm("PBEWithMD5AndDES");
+		config.setKeyObtentionIterations("1000"); // 반복할 해싱 회수
+        config.setPoolSize("1"); // 인스턴스 pool
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator"); // salt 생성 클래스
+        config.setStringOutputType("base64"); //인코딩 방식
+        encryptor.setConfig(config);
+        
+        return encryptor;
+	}
+	
+}
